f432d16176cafc1bfd9a6db6811c752a4cfaae00
Fuminghao <e165736@ie.u-ryukyu.ac.jp>
2016-11-15 09:45:55 +0900

    1st example code, but it has some troubles

--- src/main/jp/ac/uryukyu/ie/e165736/Main.java
+++ src/main/jp/ac/uryukyu/ie/e165736/Main.java
@@ -1,8 +1,5 @@
 package jp.ac.uryukyu.ie.e165736;
 
-/**
- * Created by e165736 on 2016/11/15.
- */
 public class Main { public static void main(String[] args){
     Hero hero = new Hero("勇者", 10, 5);
     Enemy enemy = new Enemy("スライム", 6, 3);
